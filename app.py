cceb0b6913d8bd69d69d111daf13591a250972bb
Hanculak <dominik.hanculak@gmail.com>
2021-01-02 23:59:54 +0100

    Fixed image identification

--- app.py
+++ app.py
@@ -1,36 +1,38 @@
 import sys
 import urllib
-from flask import Flask, jsonify
+from flask import Flask
 from flask import request
 from flask_cors import CORS
 
+
+class Person:
+  def __init__(self, pName, pImageName):
+    self.name = pName
+    self.imageName = pImageName
+
+
 app = Flask(__name__)
 CORS(app)
-imageName = ""
-personName = ""
+persons = []
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-
-@app.route('/name', methods=['GET'])
-def name():
-    message = {'name': personName}
-    return jsonify(message)
+def start():
+    return 'Server is up and running'
 
 
 @app.route('/images', methods=['POST'])
 def image():
     if request.method == 'POST':
-        global imageName, personName
-        imageName = request.get_json()["name"]
-        personName = request.get_json()["id"]
+        json = request.get_json()
+        for person in persons:
+            if person.name == json["id"]:
+                person.imageName = json["name"]
+
         print(request.get_json(), file=sys.stderr)
         if request.get_json().get("content"):
             response = urllib.request.urlopen(request.get_json()["content"])
-            with open(request.get_json()["name"], 'wb') as f:
+            with open("data/" + request.get_json()["name"], 'wb') as f:
                 f.write(response.file.read())
         return {'success': True}, 200
 
@@ -38,34 +40,32 @@ def image():
 @app.route('/questions', methods=['POST'])
 def questions():
     if request.method == 'POST':
-        global personName
         json = request.get_json()
-        json['imageName'] = imageName
-        json['personName'] = personName
-        personName = ""
+        if 'age' in json:
+            persons.append(Person(json['name'], ''))
+
+        elif 'answers' in json:
+            removable_person = None
+            for person in persons:
+                if person.name == json['name']:
+                    removable_person = person
+            persons.remove(removable_person)
+
+        else:
+            for person in persons:
+                if person.name == json['name']:
+                    json['imageName'] = person.imageName
         print(json, file=sys.stderr)
-        with open("dotaznik.txt", "a", encoding="utf-8") as f:
+        with open("data/dotaznik.txt", "a", encoding="utf-8") as f:
             f.write(str(request.get_json()) + "\n")
         return {'success': True}, 200
-
-
-# @app.route('/startingQuestions', methods=['POST'])
-# def startingQuestions():
-#     if request.method == 'POST':
-#         json = request.get_json()
-#         print(json, file=sys.stderr)
-#         with open("dotaznik.txt", "a") as f:
-#             f.write(str(request.get_json()) + "\n")
-#         return {'success': True}, 200
 
 
 @app.route('/positions', methods=['POST'])
 def positions():
     if request.method == 'POST':
-        global personName
         print(request.get_json(), file=sys.stderr)
-        personName = request.get_json()["id"]
-        with open(request.get_json()['imageName'][:-4] + ".txt", "a", encoding="utf-8") as f:
+        with open("data/" + request.get_json()['imageName'][:-4] + ".txt", "a", encoding="utf-8") as f:
             f.write(str(request.get_json()) + "\n")
         return {'success': True}, 200
 
